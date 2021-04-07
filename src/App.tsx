c69d4304de0fad3c74ac739f5f9d546c76836e28
Jake Elizondo <jake.elizondo23@gmail.com>
2021-04-07 14:57:12 -0400

    added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Todo } from './todo.model';
+import NewTodo from './components/NewTodo';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
+  };
+
+  const handleDeleteTodo = (id: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={handleAddTodo} />
+      <TodoList onDeleteTodo={handleDeleteTodo} items={todos} />
+    </div>
+  );
 };
 
 export default App;
